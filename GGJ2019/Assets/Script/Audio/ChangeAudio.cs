f525410edbdfb2ab0ea2719a2ee978dfc5710d65
Gabrieltmi <Gabrieltameomaia@gmail.com>
2019-01-25 17:48:58 -0200

    1

---
+++ GGJ2019/Assets/Script/Audio/ChangeAudio.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAudio : MonoBehaviour {
+
+
+
+	#region Variables
+	private AudioManager manager; 
+#endregion 
+//[SPACE]
+//[HEADER()]
+
+#region Unity Methods
+
+void Start () 
+	{
+		manager = GameObject.Find("AudioManager").GetComponent<AudioManager>();
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+	
+	public void MuteAudio()
+	{
+		AudioListener.pause = true;
+	}
+	public void UnmuteAudio()
+	{
+		AudioListener.pause = false;
+	}
+}
+#endregion
---
+++ GGJ2019/Assets/Script/Geral/StarTween.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarTween : MonoBehaviour {
+
+
+
+	#region Variables
+	Hashtable starScale = new Hashtable();
+	Hashtable starRotate = new Hashtable();
+#endregion 
+//[SPACE]
+//[HEADER()]
+
+#region Unity Methods
+
+void Start () 
+	{
+		starScale.Add("scale", new Vector3(0.3f, 0.3f, 0.3f));
+		starScale.Add("easetype", iTween.EaseType.easeInOutBack);
+		starScale.Add("time", 2.5f);
+		starRotate.Add("z", 15);
+		starRotate.Add("easetype", iTween.EaseType.easeInOutSine);
+		starRotate.Add("looptype", iTween.LoopType.pingPong);
+		iTween.ScaleTo(this.gameObject, starScale);
+		iTween.RotateTo(this.gameObject, starRotate);
+	}
+	
+
+	void Update () 
+	{
+		
+	}
+	
+}
+#endregion
