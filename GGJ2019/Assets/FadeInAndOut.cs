7d6470c672fb43f48d944b98af8edd1af086c4d3
Gabrieltmi <Gabrieltameomaia@gmail.com>
2019-01-26 10:01:56 -0200

    Fade

---
+++ GGJ2019/Assets/FadeInAndOut.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeInAndOut : MonoBehaviour
+{
+    public float speedFade;
+    private Color colorT;
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (colorT.a  > 0)
+         {
+            colorT = this.GetComponent<Image>().color;
+            colorT.a -= speedFade * Time.deltaTime;
+            this.GetComponent<Image>().color = colorT;
+        }
+    }
+
+    public void FadeOut()
+    {
+        if (colorT.a < 1)
+        {
+            colorT = this.GetComponent<Image>().color;
+            colorT.a += speedFade * Time.deltaTime;
+            this.GetComponent<Image>().color = colorT;
+        }
+    }
+
+}
