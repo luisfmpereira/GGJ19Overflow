f8207a92468dfec10377caec34f653c31c688044
Luis Filipe <luisf.modestop@gmail.com>
2019-01-26 11:33:24 -0200

    various updates

--- GGJ2019/Assets/Script/Geral/MoveTrigger.cs
+++ GGJ2019/Assets/Script/Geral/MoveTrigger.cs
@@ -52,7 +52,7 @@ public class MoveTrigger : MonoBehaviour
         {
             audioManager.PlaySound("TriggerDown");
             this.gameObject.transform.position = Vector3.MoveTowards(this.transform.position,
-                new Vector3(this.transform.position.x, this.transform.position.y - 0.04f, this.transform.position.z), 3);
+                new Vector3(this.transform.position.x, this.transform.position.y - 0.1f, this.transform.position.z), 3);
             iTween.MoveTo(leftDoor, openLeftDoor);
             iTween.MoveTo(rightDoor, openRightDoor);
         }
@@ -68,7 +68,7 @@ public class MoveTrigger : MonoBehaviour
             {
                 audioManager.PlaySound("TriggerUp");
                 this.gameObject.transform.position = Vector3.MoveTowards(this.transform.position, 
-                    new Vector3(this.transform.position.x, this.transform.position.y + 0.04f, this.transform.position.z), 3);
+                    new Vector3(this.transform.position.x, this.transform.position.y + 0.1f, this.transform.position.z), 3);
                 iTween.MoveTo(leftDoor, closeLeftDoor);
                 iTween.MoveTo(rightDoor, closeRightDoor);
             }
