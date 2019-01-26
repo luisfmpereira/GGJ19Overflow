b902411e95f523436cd02de202be3f86fb7dcbcd
Luis Filipe <luisf.modestop@gmail.com>
2019-01-26 15:06:49 -0200

    lots of feats

---
+++ GGJ2019/Assets/Script/Geral/Rotate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    private Transform rotateTransf;
+    [SerializeField]
+    private float rotateSpeed;
+
+    void Start()
+    {
+        rotateTransf = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotateTransf.Rotate(0,rotateSpeed * Time.deltaTime,0);
+    }
+}
---
+++ GGJ2019/Assets/Script/Geral/npcBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcBehaviour : MonoBehaviour
+{
+    private GameObject player;
+    private Transform NPC;
+
+    [SerializeField]
+    private float approachSpeed;
+    void Start()
+    {
+        NPC = GetComponent<Transform>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        NPC.transform.position = Vector2.MoveTowards(new Vector2(this.transform.position.x,this.transform.position.z),
+        new Vector2(player.transform.position.x,player.transform.position.z), approachSpeed * Time.deltaTime);
+    }
+}
