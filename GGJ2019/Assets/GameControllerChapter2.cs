ffb572f1fe96c8dfc1758ac918d765cdbe74bcf3
Gabrieltmi <Gabrieltameomaia@gmail.com>
2019-01-27 01:39:47 -0200

    wqwewq

--- GGJ2019/Assets/GameControllerChapter2.cs
+++ GGJ2019/Assets/GameControllerChapter2.cs
@@ -23,6 +23,7 @@ public class GameControllerChapter2 : MonoBehaviour
     private bool phase1;
     private bool phase2;
     private bool phase3;
+    private bool phase4;
     public GameObject hide;
     public int phase;
 
@@ -73,7 +74,7 @@ public class GameControllerChapter2 : MonoBehaviour
                     iTween.MoveTo(rightDoor, openRightDoor);
                 }
             }
-            if(phase == 2 || phase == 3)
+            else if(phase == 2 || phase == 3)
             {
                 if (triggerCount == 1 && !phase1)
                 {
@@ -96,6 +97,74 @@ public class GameControllerChapter2 : MonoBehaviour
                 else if (triggerCount == 3)
                 {
                     for (int i = 8; i < 12; i++)
+                    {
+                        yield return new WaitForSeconds(0.3f);
+                        path[i].GetComponent<Renderer>().material = useMAt;
+
+                    }
+                    hide.SetActive(true);
+                    iTween.MoveTo(leftDoor, openLeftDoor);
+                    iTween.MoveTo(rightDoor, openRightDoor);
+                }
+            }
+            else if(phase == 4)
+            {
+                if (triggerCount == 1 && !phase1)
+                {
+                    for (int i = 0; i < 6; i++)
+                    {
+                        path[i].GetComponent<Renderer>().material = useMAt;
+                        yield return new WaitForSeconds(0.3f);
+                    }
+                    phase1 = true;
+                }
+                else if (triggerCount == 2 && !phase2)
+                {
+                    for (int i = 6; i < 12; i++)
+                    {
+                        path[i].GetComponent<Renderer>().material = useMAt;
+                        yield return new WaitForSeconds(0.3f);
+                    }
+                    phase2 = true;
+                    hide.SetActive(true);
+                    iTween.MoveTo(leftDoor, openLeftDoor);
+                    iTween.MoveTo(rightDoor, openRightDoor);
+                }
+
+            }
+            else if (phase == 4)
+            {
+                if (triggerCount == 1 && !phase1)
+                {
+                    for (int i = 0; i < 4; i++)
+                    {
+                        path[i].GetComponent<Renderer>().material = useMAt;
+                        yield return new WaitForSeconds(0.3f);
+                    }
+                    phase1 = true;
+                }
+                else if (triggerCount == 2 && !phase2)
+                {
+                    for (int i = 4; i < 8; i++)
+                    {
+                        path[i].GetComponent<Renderer>().material = useMAt;
+                        yield return new WaitForSeconds(0.3f);
+                    }
+                    phase2 = true;
+                }
+                else if (triggerCount == 3)
+                {
+                    for (int i = 8; i < 12; i++)
+                    {
+                        yield return new WaitForSeconds(0.3f);
+                        path[i].GetComponent<Renderer>().material = useMAt;
+
+                    }
+       
+                }
+                else if (triggerCount == 4)
+                {
+                    for (int i = 12; i < 16; i++)
                     {
                         yield return new WaitForSeconds(0.3f);
                         path[i].GetComponent<Renderer>().material = useMAt;
