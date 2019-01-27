224f91a76fe97636196501eee17bfb4b84c8d080
Luis Filipe <luisf.modestop@gmail.com>
2019-01-27 09:46:03 -0200

    solving main menu

---
+++ GGJ2019/Assets/Script/MenuManager/MenuManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject back;
+    public GameObject mainButtons;
+    public GameObject credits;
+    public GameObject selection;
+
+    public void CreditsClick(){
+        back.SetActive(true);
+        mainButtons.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void LevelSelection(){
+        back.SetActive(true);
+        mainButtons.SetActive(false);
+        selection.SetActive(true);
+    }
+
+    public void BackClick(){
+        back.SetActive(false);
+        mainButtons.SetActive(true); 
+        credits.SetActive(false);
+        selection.SetActive(false);
+    }
+}
