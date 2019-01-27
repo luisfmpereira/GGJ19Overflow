cfb697c6d73045d99c9ab5d35b692293f4d0bfc0
Luis Filipe <luisf.modestop@gmail.com>
2019-01-26 22:56:50 -0200

    toma ae tameo

--- GGJ2019/Assets/Script/Geral/ChangeScene.cs
+++ GGJ2019/Assets/Script/Geral/ChangeScene.cs
@@ -1,19 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
     public int nextScene;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void LoadScene(string SceneName)
     {
-        
+        SceneManager.LoadScene(SceneName);
     }
 }
