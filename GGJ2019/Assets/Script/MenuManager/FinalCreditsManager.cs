74230c0bab30b720022e4cbc537fd4517d611213
Luis Filipe <luisf.modestop@gmail.com>
2019-01-27 13:42:02 -0200

    aaa

---
+++ GGJ2019/Assets/Script/MenuManager/FinalCreditsManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class FinalCreditsManager : MonoBehaviour
+{
+   public GameObject credits;
+   public GameObject thanks;
+
+    void Update(){
+        StartCoroutine("ShowCredits");
+    }
+
+
+   IEnumerator ShowCredits(){
+
+       yield return new WaitForSeconds(5);
+       credits.SetActive(true);
+       yield return new WaitForSeconds(2);
+       thanks.SetActive(true);
+       yield return new WaitForSeconds(5);
+       SceneManager.LoadScene("Main");
+
+   }
+}
